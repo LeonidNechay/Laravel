8570d92569ac5a3edbe1fccf5366d5c1072ab513
Cumparty <leonidnechay@gmail.com>
2023-03-09 16:20:38 +0200

    lab1

---
+++ lab1/resources/views/nechay.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <link rel="stylesheet" href="/app.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Nechay Leonid</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="cv">
+        <div class="name">
+            <h1>Leonid Nechay</h1>
+        </div>
+        <div class="personal-information">
+            <div class="content">
+                <h3>Personal information</h3>
+                <p>Email: nechai.leonid@student.edu.ua</p>
+                <p>Phone number: +380991234567</p>
+            </div>
+        </div>
+        <div class="education">
+            <div class="content">
+                <h3>Education</h3>
+                <p>
+                    Uzhhorod national university<br>
+                    Faculty of Mathematics and Digital Technologies<br>
+                    Specialty: system analysis
+                </p>
+            </div>
+        </div>
+        <div class="skills">
+            <div class="content">
+                <h3>Skills</h3>
+                <ul>
+                    <li>Python (aiogram, beautifulsoup4, requests)</li>
+                    <li>C#</li>
+                    <li>Vue.js (<a href="https://cakeparty-79fa8.web.app">example</a>)</li>
+                    <li>Laravel</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
